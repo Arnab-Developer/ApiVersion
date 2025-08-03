2eb817ba73bcb667c9fc1cea73bc16b587b8446a
Arnab Roy Chowdhury <arnab.roy.chowdhury.developer@gmail.com>
2025-08-03 20:30:55 +0530

    Api integration test (#2)

---
+++ test/ApiVersion.ApiTest/Endpoints/GetMessageEndpointV1Test.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace ApiVersion.ApiTest.Endpoints;
+
+[Collection("WebApplicationFactoryCollection")]
+public partial class GetMessageEndpointV1Test : IDisposable
+{
+    private readonly HttpClient _client;
+    private bool disposedValue;
+
+    public GetMessageEndpointV1Test(WebApplicationFactory<Program> factory)
+    {
+        _client = factory.CreateClient();
+    }
+
+    public void Dispose()
+    {
+        // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!disposedValue)
+        {
+            if (disposing)
+            {
+                // TODO: dispose managed state (managed objects)
+            }
+
+            // TODO: free unmanaged resources (unmanaged objects) and override finalizer
+            // TODO: set large fields to null
+            _client.Dispose();
+            disposedValue = true;
+        }
+    }
+
+    // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
+    ~GetMessageEndpointV1Test()
+    {
+        // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+        Dispose(false);
+    }
+}
---
+++ test/ApiVersion.ApiTest/Endpoints/GetMessageEndpointV2Test.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace ApiVersion.ApiTest.Endpoints;
+
+[Collection("WebApplicationFactoryCollection")]
+public partial class GetMessageEndpointV2Test : IDisposable
+{
+    private readonly HttpClient _client;
+    private bool disposedValue;
+
+    public GetMessageEndpointV2Test(WebApplicationFactory<Program> factory)
+    {
+        _client = factory.CreateClient();
+    }
+
+    public void Dispose()
+    {
+        // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!disposedValue)
+        {
+            if (disposing)
+            {
+                // TODO: dispose managed state (managed objects)
+            }
+
+            // TODO: free unmanaged resources (unmanaged objects) and override finalizer
+            // TODO: set large fields to null
+            _client.Dispose();
+            disposedValue = true;
+        }
+    }
+
+    // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
+    ~GetMessageEndpointV2Test()
+    {
+        // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+        Dispose(false);
+    }
+}
