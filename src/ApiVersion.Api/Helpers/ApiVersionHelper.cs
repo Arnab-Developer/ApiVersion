d6b8cde82223a4b3e66cd9511af06d4d768c6e57
Arnab Roy Chowdhury <arnab.roy.chowdhury.developer@gmail.com>
2025-08-03 15:48:45 +0530

    inherit from list address review comment

--- src/ApiVersion.Api/Helpers/ApiVersionHelper.cs
+++ src/ApiVersion.Api/Helpers/ApiVersionHelper.cs
@@ -4,11 +4,11 @@ internal static class ApiVersionHelper
 {
     public static AspApiVersions GetApiVersions()
     {
-        var aspApiVersions = new AspApiVersions(new List<AspApiVersion>()
+        var aspApiVersions = new AspApiVersions()
         {
             new AspApiVersion(1, 0),
             new AspApiVersion(2, 0)
-        });
+        };
 
         return aspApiVersions;
     }
